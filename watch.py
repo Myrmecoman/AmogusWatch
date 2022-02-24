f8fa1dbd54954671a851bd69df8bb7c0126c7915
Myrmecoman <45721327+Myrmecoman@users.noreply.github.com>
2022-02-24 17:58:07 +0100

    amogus dans un fchier séparé

--- watch.py
+++ watch.py
@@ -7,13 +7,16 @@ yearMonthDay = str(tm[0]) + ':' + str(tm[1]) + ':' + str(tm[2])
 hourMinuteSec = str(tm[3]) + ':' + str(tm[4]) + ':' + str(tm[5])
 
 def UpdateTime():
+    global yearMonthDay
     yearMonthDay = str(tm[0]) + ':' + str(tm[1]) + ':' + str(tm[2])
+    global hourMinuteSec
     hourMinuteSec = str(tm[3]) + ':' + str(tm[4]) + ':' + str(tm[5])
 
 def ShowTime():
     while True:
         UpdateTime()
         main.oled.fill(0)
+        global hourMinuteSec
         main.oled.text(hourMinuteSec, 0, 0)
         main.oled.show()
 
